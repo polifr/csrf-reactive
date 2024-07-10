3b463cbb9494bda6613059858b1e12057a41110d
Francesco Poli <polifr@gmail.com>
2024-07-11 01:17:15 +0200

    Aggiunta annotazione con riferimento al controller

--- src/test/java/it/poli/csrf/controller/CsrfReactiveControllerSecureTest.java
+++ src/test/java/it/poli/csrf/controller/CsrfReactiveControllerSecureTest.java
@@ -3,8 +3,10 @@ package it.poli.csrf.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@WebMvcTest(controllers = CsrfReactiveController.class)
 @ActiveProfiles(profiles = {"secure"})
 class CsrfReactiveControllerSecureTest {
 
