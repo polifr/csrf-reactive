0a49aef1545a275d937c6e644c1fa971356d72ed
Francesco Poli <polifr@gmail.com>
2024-07-11 01:12:52 +0200

    Metodo post per test

--- src/main/java/it/poli/csrf/controller/CsrfReactiveController.java
+++ src/main/java/it/poli/csrf/controller/CsrfReactiveController.java
@@ -2,6 +2,7 @@ package it.poli.csrf.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -10,6 +11,11 @@ public class CsrfReactiveController {
 
   @GetMapping("/csrf/test-get")
   public Mono<ResponseEntity<Void>> testGet() {
+    return Mono.just(ResponseEntity.ok().build());
+  }
+
+  @PostMapping("/csrf/test-post")
+  public Mono<ResponseEntity<Void>> testPost() {
     return Mono.just(ResponseEntity.ok().build());
   }
 }
